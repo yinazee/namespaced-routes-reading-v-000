6df3471f00a195f478abe1bc6754842ad33903b2
Yina Zhu <yinagit@gmail.com>
2018-08-21 19:51:33 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,18 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  #  It's really just for blog admins, and we want to set up a URL scheme to segregate admin things into their own logical space.
+  # get '/admin/stats', to: 'stats#index'
+  # get '/admin/stats', to: 'stats#index'
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'authors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
+  # or just do the below:
+
+  namespace  :admin do
+    resources :stats, only: [:index]
+  end
+
   root 'posts#index'
 end

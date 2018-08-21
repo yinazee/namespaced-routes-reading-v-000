b84c6c0475b3951ab5cb4b02331393aa205c03dd
Yina Zhu <yinagit@gmail.com>
2018-08-21 15:46:11 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   #  It's really just for blog admins, and we want to set up a URL scheme to segregate admin things into their own logical space.
   get '/admin/stats', to: 'stats#index'
-
+  get '/admin/stats', to: 'stats#index'
+  get '/admin/authors/new', to: 'authors#new'
+  get '/admin/authors/delete', to: 'authors#delete'
+  get '/admin/authors/create', to: 'authors#create'
+  get '/admin/comments/moderate', to: 'comments#moderate'
   root 'posts#index'
 end
